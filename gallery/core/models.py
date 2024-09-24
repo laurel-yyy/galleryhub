b5df14cb22ff6dacbef7389950cd1186c16d3c09
laurel-yyy <yanglujia2002@outlook.com>
2024-09-23 22:01:35 -0700

    test_database

--- gallery/core/models.py
+++ gallery/core/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Gallery(models):
+class Gallery(models.Model):
     gallery_name = models.CharField(max_length=300)
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.gallery_name
