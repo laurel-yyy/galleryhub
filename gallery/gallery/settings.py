ba5ceed79a8b00cb2d4b1504bd2fb0a9f7da3b10
laurel-yyy <yanglujia2002@outlook.com>
2024-11-24 02:07:58 -0800

    ^_^

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -24,7 +24,7 @@ AUTH_USER_MODEL = 'core.CustomUser'
 SECRET_KEY = 'django-insecure-rz+l&(mv*4+45&fppopw&85d=ac6zl%se5_de@o8rb52je_fx4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -123,11 +123,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'core', 'static')
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'core/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'core','media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
